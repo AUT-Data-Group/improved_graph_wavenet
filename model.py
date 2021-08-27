cc0f8e1173d1a14dff02a676787149ce0994c232
moghadas76 <s.m.moghadas2012@gmail.com>
2021-08-28 00:50:38 +0430

    fix

--- model.py
+++ model.py
@@ -140,7 +140,7 @@ class Temporal_Attention_layer(nn.Module):
         rhs = torch.einsum('bnlv,v->bnl', (x.permute(2, 0, 1, 3).reshape(207, 64, 13, 2),self.U_3)).contiguous()
 
         print(f"777777777777777777{lhs.shape}, {rhs.shape}")
-        product = torch.matmul(lhs, rhs.reshape(207,13,64))
+        product = torch.matmul(lhs.reshape(64,13,207), rhs.reshape(64,207,13))
 
         E = torch.matmul(self.V_e,
                    F.sigmoid(product + self.b_e)
